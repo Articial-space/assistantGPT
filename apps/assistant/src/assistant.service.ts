9cf75c28e83bb2cabe3d57228e5890dc2ee5c31f
Articial-space <kevinkhoa0203@outlook.com>
2025-05-18 12:10:18 +0000

    fourth-test

--- apps/assistant/src/assistant.service.ts
+++ apps/assistant/src/assistant.service.ts
@@ -6,10 +6,15 @@ import OpenAI from 'openai';
 
 @Injectable()
 export class AssistantService {
+<<<<<<< HEAD
   constructor(private readonly configService: ConfigService) {}
   private readonly OpenAI = new OpenAI({
     apiKey: this.configService.get('OPENAI_API_KEY')
   })
+=======
+  constructor(private readonly configService: ConfigService, private readonly httpService: HttpService) {}
+
+>>>>>>> c405b77 (fourth-test)
   async answerResquest(@Payload() messageDto: MessageDto) {
     const response = await this.OpenAI.responses.create({
       model: 'gpt-4.1',
